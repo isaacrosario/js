b8209adb4fe393907008522c12632cbc508902ae
Isaac <isaacrosariomarrero@gmail.com>
2018-04-06 16:59:24 -0400

    Anadiendo librerias

---
+++ fadeIn.js
@@ -0,0 +1,17 @@
+<script src="js/jquery-3.3.1.js"> //http://code.jquery.com/jquery-latest.min.js libreria online
+</script>
+			<script>
+				$(function(){
+
+
+					$("#logotipo").hide().fadeIn(6000);
+					$("#slideshow").hide().fadeIn(12000);
+					$("#bienvenidos").hide().fadeIn(24000);
+					$("#contenido").hide().fadeIn(48000);
+					$("#acerca-de").hide().fadeIn(60000);
+					$("aside").hide().fadeIn(70000);
+					$("footer").hide().fadeIn(80000);
+
+				});
+				
+			</script>
